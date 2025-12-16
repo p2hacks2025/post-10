3e36e63474c242a0f3a552c36bfa615a8820531e
Jagri495 <b1023128@fun.ac.jp>
2025-12-16 12:55:32 +0900

    バックエンドの要件を簡単に書いた

--- backend/test_jagri.py
+++ backend/test_jagri.py
@@ -1,14 +1,28 @@
 Post_detail=[]#ポストのいいねの数と悪いねの数
 Appear_post=[]#表示するポスト
+class User:
+    def __init__(self,goodnumber,badnunber):#(ユーザー)
+        self.posts=[]
+        self.pushGood=False #ポストにいいね押した判定
+        self.pushBad=False #ポストに悪いねを押した判定
+    def PushGood(self):#ユーザーがいいねや悪いねを押した数
+        if self.pushGood==True:
+            self.goodnunber+=1
+        if self.pushBad==True:
+            self.badnunber+=1
+
 class Post:
-    def __init__(self,good,bad):
-        self.good=0  #ポストのいいねの数 
-        self.bad=0 #ポストの悪いねの数
+    def __init__(self,user,good,bad):#(ポスト,ユーザー,いいねの数,悪いねの数)
+        self.user=user
+        self.good=good  #ポストのいいねの数 
+        self.bad=bad #ポストの悪いねの数
+        
     def savePost(self):#ポストのいいねの数と悪いねの数を記録
-        Post_detail.insert[self,self.good,self.bad]#Post_detailに保存
-        Appear_post.insert(self)#Appear_postに保存
+        Post_detail.append([self,self.user,self.good,self.bad])#Post_detailに保存
+        Appear_post.append(self)#Appear_postに保存
         if len(Appear_post)>20:#Appear_postの要素数が20を超える場合
             Appear_post.pop(0)#一番前の値を削除
+        self.user.posts.append(self)#Userのpostsに保存
     def calcPost(self):#postの評価を計算
         n=10#基準の数
         m=0.5#基準の数
